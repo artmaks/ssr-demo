5455833047d4541187c427ea886e697d99bada86
artmaks <temamaksimov@gmail.com>
2019-03-11 22:45:58 +0300

    node externals

--- webpack/server.config.js
+++ webpack/server.config.js
@@ -1,5 +1,8 @@
+var nodeExternals = require('webpack-node-externals');
+
 module.exports = {
     target: 'node',
+    externals: [nodeExternals()],
     entry: './src/server/index.js',
     output: {
         filename: 'ssr.bundle.js',
